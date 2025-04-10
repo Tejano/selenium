c9f11e901a819b5081c1e6d3fc2f5789cb2abc97
Humberto Escobedo <hescobedo@hotmail.com>
2025-04-10 17:00:06 -0500

    Update SQLFindJobsWithoutOwners.sql

--- SQLFindJobsWithoutOwners.sql
+++ SQLFindJobsWithoutOwners.sql
@@ -43,3 +43,48 @@ WHERE
     js.subsystem = 'SSIS'
     AND js.command LIKE '%\SSISDB\folderName\%'  -- targeting  folder
     AND (j.owner_sid IS NULL OR sp.sid IS NULL);
+
+
+SELECT 
+    j.name AS job_name,
+    j.job_id,
+    j.owner_sid,
+    SUSER_SNAME(j.owner_sid) AS owner_name,
+    js.step_id,
+    js.step_name,
+    js.command AS step_command,
+    CASE 
+        WHEN sp.sid IS NULL THEN 'ORPHANED OWNER'
+        ELSE 'VALID OWNER'
+    END AS owner_status
+FROM 
+    msdb.dbo.sysjobs AS j
+LEFT JOIN 
+    sys.server_principals AS sp ON j.owner_sid = sp.sid
+INNER JOIN 
+    msdb.dbo.sysjobsteps AS js ON j.job_id = js.job_id
+WHERE 
+    js.subsystem = 'SSIS'
+    AND js.command LIKE '%\SSISDB\commission\%' 
+    AND sp.sid IS NULL  -- means the login no longer exists in the server
+ORDER BY 
+    j.name;
+
+
+-- Set new valid owner SID
+DECLARE @NewOwnerSID VARBINARY(85);
+
+SELECT @NewOwnerSID = sid
+FROM sys.server_principals 
+WHERE name = 'your_domain\new_owner_login';  -- replace with your login
+
+-- Update orphaned job owners
+UPDATE j
+SET j.owner_sid = @NewOwnerSID
+FROM msdb.dbo.sysjobs AS j
+LEFT JOIN sys.server_principals AS sp ON j.owner_sid = sp.sid
+INNER JOIN msdb.dbo.sysjobsteps AS js ON j.job_id = js.job_id
+WHERE 
+    js.subsystem = 'SSIS'
+    AND js.command LIKE '%\SSISDB\commission\%' 
+    AND sp.sid IS NULL;
